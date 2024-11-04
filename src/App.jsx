6ad2eab5fbac140b0a5ddfd23fbf1c16c0018e27
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-11-04 17:50:25 +0100

    Added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Button, Typography, useTheme } from "@mui/material";
 
 function App() {
   const theme = useTheme();
 
   return (
-    <Box p={6}>
+    <Box p={3}>
       <Typography variant="h4" fontWeight={"bold"} textAlign={"center"}>
         Hello world!
       </Typography>
+
+      <Box
+        maxWidth={"600px"}
+        margin="auto"
+        border={`1px solid ${theme.palette.grey[400]}`}
+        borderRadius={1}
+        p={2}
+      >
+        <Typography mb={2} textAlign={"center"} as={"h2"} variant="h5">
+          Header
+        </Typography>
+
+        <Box
+          display={"flex"}
+          flexDirection={"row"}
+          justifyContent={"center"}
+          flexWrap={"wrap"}
+          gap={2}
+        >
+          <Button variant="contained" color="success">
+            Proceed
+          </Button>
+          <Button variant="outlined" color="error">
+            Cancel
+          </Button>
+        </Box>
+      </Box>
     </Box>
   );
 }
