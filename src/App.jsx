7a3f64820c618ff0141ed394ef1b0be15f648f80
Marc Marina <mmarina@findmypast.com>
2024-11-14 17:09:44 +0100

    Added Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,39 @@
 import React from "react";
 
-import { Box, Button, Container, Typography, useTheme } from "@mui/material";
+import { Box, colors, useTheme } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 
 function App() {
   const theme = useTheme();
 
   return (
-    <Container>
-      <Typography variant="h4" fontWeight={"bold"} textAlign={"center"}>
-        Hello world!
-      </Typography>
-
-      <Box
-        border={`1px solid ${theme.palette.grey[400]}`}
-        borderRadius={1}
-        p={2}
+    <Box p={2} display="flex" justifyContent={"center"}>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          flexGrow: 1,
+          maxWidth: theme.breakpoints.values.xl,
+        }}
       >
-        <Typography mb={2} textAlign={"center"} as={"h2"} variant="h5">
-          Header
-        </Typography>
-
-        <Box
-          display={"flex"}
-          flexDirection={"row"}
-          justifyContent={"center"}
-          flexWrap={"wrap"}
-          gap={2}
-        >
-          <Button variant="contained" color="success">
-            Proceed
-          </Button>
-          <Button variant="outlined" color="error">
-            Cancel
-          </Button>
-        </Box>
-      </Box>
-    </Container>
+        {Array(6)
+          .fill(0)
+          .map((_, i) => {
+            return (
+              <Grid key={i} size={[12, 6, 3]}>
+                <Box
+                  sx={{
+                    bgcolor: colors.red[(i + 1) * 100],
+                    p: 8,
+                  }}
+                >
+                  size=8
+                </Box>
+              </Grid>
+            );
+          })}
+      </Grid>
+    </Box>
   );
 }
 
